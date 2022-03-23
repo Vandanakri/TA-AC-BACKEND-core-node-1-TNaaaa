0519a6926692eee3ab4442db886d11627b4bd354
vandana <vandana123annusah@gmail.com>
2022-03-23 14:50:14 +0530

    block-BJaaap

--- block-BNaaap/server.js
+++ block-BNaaap/server.js
@@ -3,10 +3,12 @@ var http = require('http');
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
+  console.log(req.method)
   res.statusCode = 201;
-  res.end('Hello Vandana')
+  res.setHeader('Content-type','text/html');
+  res.end('<h2>Hello Vandana</h1>')
 }
 
 server.listen(4444, () => {
-  console.log('server listening on port 4000')
+  console.log('server listening on port 4444')
 })
